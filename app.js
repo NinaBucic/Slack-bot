a2220368b20ecacfb467dc78a7e5c48be97c856d
Nina Bucic <bucicnina@gmail.com>
2025-03-21 15:10:59 +0100

    Set up inital event

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const { App } = require("@slack/bolt");
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+});
+
+app.message("hello", async ({ message, say }) => {
+  await say(`Hey there <@${message.user}>!`);
 });
 
 (async () => {
-  await app.start(process.env.PORT || 3000);
+  await app.start();
 
   app.logger.info("⚡️ Slack bot is running!");
 })();
